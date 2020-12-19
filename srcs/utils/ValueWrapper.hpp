a60a4d925749b868d4c2bb14ed26b4e480a3836e
Phantomas <phantomas@phantomas.xyz>
2020-12-19 23:36:08 +0100

    [REFACT] Reworked ValueWrapper
    CHANGELOG:
    - Rework ValueWrapper
    - Removed gcc only extensions.
    - Added documentation.
    - Added rvalue support

--- srcs/utils/ValueWrapper.hpp
+++ srcs/utils/ValueWrapper.hpp
@@ -3,7 +3,8 @@
 **
 ** \author Phantomas <phantomas@phantomas.xyz>
 ** \date Created on: 2020-05-08 23:33
-** \date Last update: 2020-05-10 19:24
+** \date Last update: 2020-05-30 15:37
+** \copyright GNU Lesser Public Licence v3
 */
 
 #ifndef utils_ValueWrapper_hpp__
@@ -19,108 +20,161 @@
 
 namespace clonixin::utils::value {
     namespace _internals {
-        template <typename T> struct __type_of_member {
-            using type = T;
-        };
-
-        template <class T, typename M> struct __type_of_member<M T::*> {
-            using type = M;
-        };
-
+        /**
+        ** \brief Meta-programming base of Direct and Indirect value holding
+        ** types.
+        **
+        ** \tparam T A valid type.
+        */
         template <typename T>
         struct __value_wrap {
             using type = T;
         };
 
+        /**
+        ** \brief Meta-programming struct that discriminate between value
+        ** holding and normal types.
+        **
+        ** This fallback version is instantiated for non value holding types.
+        **
+        ** \tparam T The type that we want to unwrap, if needed.
+        */
         template <typename T, typename = std::void_t<>>
         struct __value_unwrapper {
+            /**
+            ** \brief The unwrapped type, or std::shared_ptr<T> if it was not a
+            ** value holding type.
+            */
             using type = std::shared_ptr<T>;
 
+            /**
+            ** \brief A function that return the value, or use the container to
+            ** build it, it T was not a value holding type.
+            */
             static type value(Container const &c) { return c.getInstance<T>(); }
         };
 
+        /**
+        ** \brief Meta-programming struct that discriminate between value
+        ** holding and normal types.
+        **
+        ** This fallback version is instantiated for non value holding types.
+        **
+        ** \tparam T The type that we want to unwrap, if needed.
+        */
+        template <typename T>
+        struct __value_unwrapper<T, std::void_t<
+            std::enable_if_t<std::is_rvalue_reference_v<T>, T>
+        >> {
+            /**
+            ** \brief The unwrapped type, or std::shared_ptr<T> if it was not a
+            ** value holding type.
+            */
+            using type = T;
+
+            /**
+            ** \brief A function that return the value, or use the container to
+            ** build it, it T was not a value holding type.
+            */
+            static type value(Container const &c) { return c.getInstance<T>(); }
+        };
+
+        /**
+        ** \brief Meta-programming struct that discriminate between value
+        ** holding and normal types.
+        **
+        ** This specialisation is used if T is a value holding type, that has
+        ** __value_wrap as a base class. It then retrieve the wrapped type and
+        ** declare it with a typedef as type.
+        **
+        ** It's value function returns the wrapped type, instead of using the
+        ** container to build it.
+        **
+        ** \tparam T The type that we want to unwrap, if needed.
+        */
         template <typename T>
         struct __value_unwrapper<T, std::void_t<
             std::enable_if_t<std::is_base_of_v<__value_wrap<typename T::type>, T>>
         >> {
+            /**
+            ** \brief The unwrapped type.
+            */
             using type = typename T::type;
 
+            /**
+            ** \brief A function that return the wrapped value.
+            **
+            */
             static constexpr type value(Container const &c) { return T::value; }
         };
     }
 
-    template <auto val, typename T = decltype(val)> struct Direct: public _internals::__value_wrap<T> {
+    /**
+    ** \brief Direct value holding type.
+    **
+    ** This type contains the wrapped value, and represent it's type as well as
+    ** it's value.
+    */
+    template <auto val, typename T = decltype(val)>
+    struct Direct: public _internals::__value_wrap<T> {
+        /**
+        ** \brief Wrapped type.
+        */
         using type = T;
+
+        /**
+        ** \brief Wrapped value.
+        */
         static constexpr type value = val;
     };
 
-    template <typename T, typename R = decltype(std::declval<T>().value)> struct Indirect : public _internals::__value_wrap<R> {
+    /**
+    ** \brief Indirect value holding type.
+    **
+    ** This type is mainly used for types that cannot be used as templates
+    ** parameters. The type T must be constructible and have a public value
+    ** attribute. The actual type is deduced by using decltype on the value
+    ** attribute, if the parameter is left unspecified.
+    **
+    ** \tparam T A wapper type that contains the value, and must be constructible.
+    ** \tparam R The type of the wrapped value.
+    */
+    template <typename T, typename R = decltype(std::declval<T>().value)>
+    struct Indirect : public _internals::__value_wrap<R> {
+        /**
+        ** \brief Wrapped value type.
+        */
         using type = R;
+
+        /**
+        ** \brief Wrapped value.
+        */
         static constexpr type value = T().value;
     };
 
-#ifdef __COMP_GCC__
-    namespace _internals {
-        template <char... chars>
-        using __litt_string = std::integer_sequence<char, chars...>;
-    }
+    /**
+    ** \name Direct Partial Specialization
+    */
+    /**@{*/
+    template <bool val> using Bool = Direct<val, bool>;
 
-    template <typename T, T... chars>
-    constexpr _internals::__litt_string<chars ...> operator ""_str() { return {}; }
+    template <short val> using Short = Direct<val, short>;
+    template <unsigned short val> using UShort = Direct<val, unsigned short>;
+    template <int val> using Int = Direct<val, int>;
+    template <unsigned int val> using UInt = Direct<val, unsigned int>;
+    template <long val> using Long = Direct<val, long>;
+    template <unsigned long val> using ULong = Direct<val, unsigned long>;
+    template <long long val> using LongLong = Direct<val, long long>;
+    template <unsigned long long val> using ULongLong = Direct<val, unsigned long long>;
 
-    template <typename>
-    struct _string_impl {};
+    template <char val> using Char = Direct<val, char>;
+    template <signed char val> using SChar = Direct<val, signed char>;
+    template <unsigned char val> using UChar = Direct<val, unsigned char>;
 
-    template <char... chars>
-        struct _string_impl<_internals::__litt_string<chars...>> {
-            static constexpr std::string value() { return std::string{chars...}; };
-        };
-
-    template <typename>
-    struct String {};
-
-    template <char... chars>
-    struct String<_internals::__litt_string<chars...>> : public _internals::__value_wrap<std::string> {
-        using type = std::string;
-        static std::string value;
-    };
-
-    template<char... chars>
-    std::string String<_internals::__litt_string<chars...>>::value = _string_impl<_internals::__litt_string<chars...>>::value();
-#else
-    namespace _internals {
-        template <char... chars>
-        using __litt_string = std::integer_sequence<char, chars...>;
-    }
-
-    template <typename T, T... chars>
-    constexpr _internals::__litt_string<chars ...> operator ""_str() { return {}; }
-
-    template <typename T>
-        struct String {
-            static_assert(!std::is_same_v<T, T>, "String value_type is unsupported by clang. Use Indirect instead.");
-        };
-#endif
-
-#define X_VALUEDECLARATOR(Type, Name) template <Type val> using Name = Direct<val, Type>
-    X_VALUEDECLARATOR(bool, Bool);
-
-    X_VALUEDECLARATOR(short, Short);
-    X_VALUEDECLARATOR(unsigned short, UShort);
-    X_VALUEDECLARATOR(int, Int);
-    X_VALUEDECLARATOR(unsigned int, UInt);
-    X_VALUEDECLARATOR(long, Long);
-    X_VALUEDECLARATOR(unsigned long, ULong);
-    X_VALUEDECLARATOR(long long, LongLong);
-    X_VALUEDECLARATOR(unsigned long long, ULongLong);
-
-    X_VALUEDECLARATOR(char, Char);
-    X_VALUEDECLARATOR(signed char, SChar);
-    X_VALUEDECLARATOR(unsigned char, UChar);
-    X_VALUEDECLARATOR(wchar_t, WChar);
-    X_VALUEDECLARATOR(char16_t, Char16);
-    X_VALUEDECLARATOR(char32_t, Char32);
-#undef X_VALUEDECLARATOR
+    template <wchar_t val> using WChar = Direct<val, wchar_t>;
+    template <char16_t val> using Char16 = Direct<val, char16_t>;
+    template <char32_t val> using Char32 = Direct<val, char32_t>;
+    /**@}*/
 }
 
 #endif
